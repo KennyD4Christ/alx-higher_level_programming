1699cdce1674dccbc2841f6304562fc4bfeb0fec
KennyD4Christ <kennyd4christ@gmail.com>
2024-01-02 16:21:50 +0100

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,80 +1,29 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- *  * add_nodeint - Adds a new node at the beginning of a linked list.
- *   * @head: Pointer to the head of the list.
- *    * @n: Value to set in the new node.
- *     *
- *      * Return: Address of the new node or NULL on failure.
- */
-
-listint_t *add_nodeint(listint_t *head, int n)
-{
-listint_t *new_node = malloc(sizeof(listint_t));
-if (!new_node)
-return (NULL);
-new_node->n = n;
-new_node->next = head;
-return (new_node);
-}
-
-/**
- *  * print_listint - Prints the elements of a linked list.
- *   * @head: Pointer to the head of the list.
- */
-
-void print_listint(const listint_t *head)
-{
-while (head != NULL)
-{
-printf("%d", head->n);
-if (head->next != NULL)
-{
-printf(" -> ");
-}
-head = head->next;
-}
-printf("\n");
-}
-
-/**
- *  * free_listint - Frees the memory allocated for a linked list.
- *   * @head: Pointer to the head of the list.
- */
-
-void free_listint(listint_t *head)
-{
-listint_t *current, *next;
-current = head;
-while (current != NULL)
-{
-next = current->next;
-free(current);
-current = next;
-}
-}
-
-/**
- *  * check_cycle - Check if a singly linked list has a cycle.
- *   * @list: A pointer to the head of the linked list.
- *    * Return: 0 if there is no cycle, 1 if there is a cycle.
+ *
+ *  * check_cycle - checks if a linked list contains a cycle
+ *
+ *   * @list: linked list to check
+ *
+ *    *
+ *
+ *     * Return: 1 if the list has a cycle, 0 if it doesn't
+ *
  */
 
 int check_cycle(listint_t *list)
 {
-listint_t *tortoise, *hare;
-if (list == NULL)
+listint_t *slow = list;
+listint_t *fast = list;
+if (!list)
 return (0);
-tortoise = list;
-hare = list->next;
-while (tortoise != hare)
+while (slow && fast && fast->next)
 {
-if (hare == NULL || hare->next == NULL)
-return (0);
-tortoise = tortoise->next;
-hare = hare->next->next;
-}
+slow = slow->next;
+fast = fast->next->next;
+if (slow == fast)
 return (1);
+}
+return (0);
 }
