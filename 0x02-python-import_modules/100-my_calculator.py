7405497060951ae41566edb5c5b841ed68047606
KennyD4Christ <kennyd4christ@gmail.com>
2024-01-04 14:04:35 +0100

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from sys import argv
     num_args = len(argv)
 
-    if nur_args != 4:
+    if num_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
