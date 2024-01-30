6e137b1465a6a6da1b65b633f12e7b378a76948a
KennyD4Christ <kennyd4christ@gmail.com>
2024-01-30 13:38:02 +0100

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 class LockedClass:
-    __slots__ = ['first_name']
+    def __setattr__(self, name, value):
+        if name == 'first_name':
+            # Allow assignment of 'first_name' attribute
+            object.__setattr__(self, name, value)
+        else:
+            raise AttributeError(
+                "'LockedClass' object has no attribute '{}'".format(name)
+            )
