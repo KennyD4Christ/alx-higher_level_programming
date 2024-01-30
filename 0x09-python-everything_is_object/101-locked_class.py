d9f2c3bfc8e5d5f8c2b7ca9e57258ffcd034fb04
KennyD4Christ <kennyd4christ@gmail.com>
2024-01-30 13:48:10 +0100

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 class LockedClass:
-    """A class that prevents the dynamic creation of new instance attributes,
-    except 'first_name'."""
-    __slots__ = ['first_name']
+    """A class that restricts attribute assignment, allowing only 'first_name'.
+
+    Attributes:
+        None
+    """
+
+    def __setattr__(self, name, value):
+        """Control attribute assignment for LockedClass.
+
+        Args:
+            name (str): The name of the attribute being assigned.
+            value: The value being assigned to the attribute.
+
+        Raises:
+            AttributeError: If the attribute being assigned is not 'first_name'.
+        """
