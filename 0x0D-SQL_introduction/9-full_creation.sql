a4e03af2a2d6ec173912fd2a811fac37f8d81d3d
KennyD4Christ <kennyd4christ@gmail.com>
2024-03-12 16:07:25 +0100

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,12 @@
 -- Create the table if it doesn't exist
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+CREATE TABLE IF NOT EXISTS `second_table` (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
 -- Add multiple rows to the table
-INSERT INTO hbtn_0c_0.second_table (id, name, score)
+INSERT INTO `second_table` (id, name, score)
 VALUES (1, 'John', 10),
        (2, 'Alex', 3),
        (3, 'Bob', 14),
