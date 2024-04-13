74debddb67fa28b8da903553693394676eb55b56
KennyD4Christ <kennyd4christ@gmail.com>
2024-04-13 15:47:44 +0100

    101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -8,7 +8,7 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
-from relationship_city import City
+from relationship_city import City  # noqa
 
 
 if __name__ == "__main__":
